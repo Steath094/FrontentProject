ff1958a6a57f0799a36d4ebf6057a5b7cf103fc2
Sam <armandawoodkhan@gmail.com>
2024-12-29 13:31:53 +0530

    Build Frontend Template

--- Book Notes/index.js
+++ Book Notes/index.js
@@ -7,10 +7,56 @@ app.set('view engine', 'ejs');
 app.use(express.static("public"))
 app.use(express.json()) // body-parser is now deprecated as of Express 4.16+
 app.use(express.urlencoded({ extended: true }))
-app.get('/', (req, res)=>{
-    res.render("index.ejs");
-});
 
+
+//routes
+app.get('/', (req, res) => {
+    res.render('index', {
+        books: [
+          {
+            title: 'All The Light We Cannot See',
+            author: 'Anthony Doerr',
+            votes: '1,988,288',
+            rating: '1',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus morbi eleifend enim, tristique.',
+            image: 'https://pixlr.com/images/generator/how-to-generate.webp'
+          },
+          {
+            title: 'Where The Crawdads Sing',
+            author: 'Delia Owens',
+            votes: '1,988,288',
+            rating: '4',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus morbi eleifend enim, tristique.',
+            image: 'https://cdn.pixabay.com/photo/2024/01/31/18/39/vibrant-8544591_1280.png'
+          },
+          {
+            title: 'Rich People Problems',
+            author: 'Kevin Kwan',
+            votes: '1,988,288',
+            rating: '3',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus morbi eleifend enim, tristique.',
+            image: 'rich-people-problems.jpg'
+          },
+          {
+            title: 'Where The Crawdads Sing',
+            author: 'Delia Owens',
+            votes: '1,988,288',
+            rating: '0',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus morbi eleifend enim, tristique.',
+            image: 'https://cdn.pixabay.com/photo/2024/01/31/18/39/vibrant-8544591_1280.png'
+          },
+          {
+            title: 'Rich People Problems',
+            author: 'Kevin Kwan',
+            votes: '1,988,288',
+            rating: '2',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus morbi eleifend enim, tristique.',
+            image: 'rich-people-problems.jpg'
+          }
+
+        ]
+      });
+  });
 app.listen(PORT,(error)=>{
     if(!error)
         console.log("Server is Successfully Running, and App is listening on port "+ PORT)
